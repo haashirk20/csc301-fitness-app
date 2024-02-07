05951933f870f801f7d5c53aef8c08a8638691b6
haashirk2003 <haashirk2003@gmail.com>
2024-02-07 18:28:42 -0500

    User signup functionality #6 - Finished base signup function for DB

--- app/routes.py
+++ app/routes.py
@@ -34,3 +34,29 @@ def login():
         # we don't have to specify which one of email or password failed,
         # since that could be a security risk (reveals if email exists)
         return {"message": "incorrect email or password"}, 401
+    
+#basic signup route
+@app.route("/api/signup", methods=["POST"])
+def signup():
+    if "user_id" in session:
+        return {"message": "user already signed in"}
+
+    user_email = request.args.get("email", "").lower()
+    user_pass = request.args.get("password", "")
+
+    #TODO implement birthday storing
+    #user_bday = request.args.get("birthday", "")
+
+    if user_email == "":
+        return {"message": "email missing"}, 400
+    elif user_pass == "":
+        return {"message": "password missing"}, 400
+
+    #check if email is valid
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", user_email):
+        return {"message": "invalid email"}, 400
+
+    user = models.User(user_email, user_pass)
+    user.save_to_db()
+
+    return redirect(url_for("home"))
