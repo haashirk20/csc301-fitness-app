d45bd1d656b0fe3ee7fda83770d278a3e69a5c00
envoamr <66641746+envoamr@users.noreply.github.com>
2024-02-07 21:07:13 -0500

    Ticket #7 Updated login to use the same attributes as signup

--- app/models.py
+++ app/models.py
@@ -4,36 +4,59 @@ import json
 
 
 class User:
-    def __init__(self, email, hash, name, age):
+    def __init__(self, id=0, name="", password="", email="", age=0):
+        self.id = id  # firebase id
         self.name = name
         self.age = age
         self.email = email
-        self.hash = bcrypt.generate_password_hash(hash, 10).decode()
+        self.password = password
+        if len(self.password) > 0:
+            self.hash = bcrypt.generate_password_hash(password, 10).decode()
 
-    def save_to_db(self):
+    def signup(self):
         ref = db.reference("users")
-        new_user_ref = ref.push({"email": self.email, "hash": self.hash, "age": self.age, "name": self.name})
+        # user must be unique
+        user_dict = ref.order_by_child("name").equal_to(self.name).get()
+        if len(user_dict) == 0:
+            new_user_ref = ref.push(
+                {
+                    "email": self.email,
+                    "hash": self.hash,
+                    "age": self.age,
+                    "name": self.name,
+                }
+            )
+            return True
+        else:
+            return False
 
-        return new_user_ref.key
+    # checks whether user exists and password is correct
+    # returns True on success, False on failure
+    def login(self):
+        ref = db.reference("users")
+        user_dict = ref.order_by_child("name").equal_to(self.name).get()
+        user = next(iter(user_dict.items()), None)
+
+        if user and bcrypt.check_password_hash(user[1]["hash"], self.password):
+            self.id = user[0]
+            self.name = user[1]["name"]
+            self.email = user[1]["email"]
+            self.hash = user[1]["hash"]
+            return True
+        else:
+            return False
+
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.name
 
     def get_email(self):
         return self.email
 
-    @staticmethod
-    def login(form_email, form_pass):
-        ref = db.reference("users")
-        user_dict = ref.order_by_child("email").equal_to(form_email).get()
-        user = next(iter(user_dict.items()), None)
-
-        if user:
-            # user found, get their email/hash
-            user_id = user[0]
-            user_hash = user[1]["hash"]
-            form_hash = bcrypt.generate_password_hash(form_pass, 10)
-            if user_hash == form_hash:
-                return user_id
-        else:
-            return None
+    def get_age(self):
+        return self.age
 
     @staticmethod
     def get_all_users():
@@ -41,7 +64,6 @@ class User:
 
         users = ref.get()
 
-        # Convert the dictionary of users to a list
         user_list = [] if users is None else list(users.values())
 
         return user_list
--- app/routes.py
+++ app/routes.py
@@ -4,38 +4,31 @@ import re
 from app import models
 
 
-@app.route("/")
-def home():
-    if "user_id" in session:
-        return f"Hey, {session['user_id']}! You are authenticated."
-    else:
-        return "Hey, anonymous user! You are not authenticated."
-
-
 @app.route("/api/login", methods=["POST"])
 def login():
-    if "user_id" in session:
+    if "user" in session:
         return {"message": "user already signed in"}
 
-    user_email = request.args.get("email", "").lower()
+    user_name = request.args.get("name", "")
     user_pass = request.args.get("password", "")
 
-    if user_email == "":
-        return {"message": "email missing"}, 400
+    if user_name == "":
+        return {"message": "name missing"}, 400
     elif user_pass == "":
         return {"message": "password missing"}, 400
 
-    user = models.User.login(user_email, user_pass)
+    user = models.User(name=user_name, password=user_pass)
 
-    if user:
-        session["user_id"] = user
-        return redirect(url_for("home"))
+    if user.login():
+        session["user"] = user.__dict__  # save user model or user id?
+        return {"message": "user signed in"}, 200
     else:
         # we don't have to specify which one of email or password failed,
         # since that could be a security risk (reveals if email exists)
         return {"message": "incorrect email or password"}, 401
-    
-#basic signup route
+
+
+# basic signup route
 @app.route("/api/signup", methods=["POST"])
 def signup():
     if "user_id" in session:
@@ -55,11 +48,15 @@ def signup():
     elif user_pass == "":
         return {"message": "password missing"}, 400
 
-    #check if email is valid
+    # check if email is valid
     if not re.match(r"[^@]+@[^@]+\.[^@]+", user_email):
         return {"message": "invalid email"}, 400
 
-    user = models.User(user_email, user_pass, user_name, user_age)
-    user.save_to_db()
+    user = models.User(
+        name=user_name, password=user_pass, email=user_email, age=user_age
+    )
 
-    return {"message": "user created"}, 200
+    if user.signup():
+        return {"message": "user created"}, 200
+    else:
+        return {"message": "user already exists"}, 400
