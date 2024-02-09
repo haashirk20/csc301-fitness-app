9551ff0dbf5d4d37d4646647357d28b78c36ab51
Zandercat <42007489+Zandercat@users.noreply.github.com>
2024-02-08 22:10:35 -0500

    added submit event listener to BMI page that sends POST requests

--- website/js/bmi.js
+++ website/js/bmi.js
@@ -1,10 +1,10 @@
-const signup_Form = document.getElementById('suForm');
+const bmi_Form = document.getElementById('bmiForm');
 
-if (signup_Form) {
-    signup_Form.addEventListener('submit', async function (e) {
+if (bmi_Form) {
+    bmi_Form.addEventListener('submit', async function (e) {
         e.preventDefault();
-        const formData = new FormData(signup_Form).entries()
-        const response = await fetch('http://127.0.0.1:5000/api/signup', {
+        const formData = new FormData(bmi_Form).entries()
+        const response = await fetch('http://127.0.0.1:5000/api/bmi', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(Object.fromEntries(formData))
--- website/js/login.js
+++ website/js/login.js
@@ -4,7 +4,7 @@ if (login_Form) {
     login_Form.addEventListener('submit', async function (e) {
         e.preventDefault();
         const formData = new FormData(login_Form).entries()
-        const response = await fetch('https://127.0.0.1:5000/api/login', {
+        const response = await fetch('http://127.0.0.1:5000/api/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(Object.fromEntries(formData))
