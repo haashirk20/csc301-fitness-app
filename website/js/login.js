8e90a683f7ba69d7dac5eb61246cb4bc6d89a78f
Zandercat <42007489+Zandercat@users.noreply.github.com>
2024-02-09 04:07:26 -0500

    calories

--- website/js/login.js
+++ website/js/login.js
@@ -8,7 +8,8 @@ if (login_Form) {
         const response = await fetch('http://127.0.0.1:5000/api/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(Object.fromEntries(formData))
+            body: JSON.stringify(Object.fromEntries(formData)),
+			credentials: include,
         });
     
         const result = await response.json();
--- website/js/signup.js
+++ website/js/signup.js
@@ -8,7 +8,8 @@ if (signup_Form && errM) {
         const response = await fetch('http://127.0.0.1:5000/api/signup', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(Object.fromEntries(formData))
+            body: JSON.stringify(Object.fromEntries(formData)),
+			credentials: include,
         });
     
         const result = await response.json();
