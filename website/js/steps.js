665f11edbb89e01a7a8523a6b04320d44a1e6174
Steven Wang <stev.wang@mail.utoronto.ca>
2024-02-25 15:11:27 -0500

    Added month chart

--- website/js/steps.js
+++ website/js/steps.js
@@ -1,11 +1,72 @@
-const Steps = ["1/11","2/11","3/11","4/11","5/11","6/11","7/11","8/11","9/11","10/11"];
-const date = [860,1140,1060,1060,null,1110,1330,2210,7830,2478];
+const date = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const month =  ["2024-02-01",
+                "2024-02-02",
+                "2024-02-03",
+                "2024-02-04",
+                "2024-02-05",
+                "2024-02-06",
+                "2024-02-07",
+                "2024-02-08",
+                "2024-02-09",
+                "2024-02-10",
+                "2024-02-11",
+                "2024-02-12",
+                "2024-02-13",
+                "2024-02-14",
+                "2024-02-15",
+                "2024-02-16",
+                "2024-02-17",
+                "2024-02-18",
+                "2024-02-19",
+                "2024-02-20",
+                "2024-02-21",
+                "2024-02-22",
+                "2024-02-23",
+                "2024-02-24",
+                "2024-02-25",
+                "2024-02-26",
+                "2024-02-27",
+                "2024-02-28",
+                "2024-02-29"]
+const steps = [1060,1121,1110,null,2210,3830,2478];
+const month_steps = [3796,
+                    3824,
+                    3312,
+                    2171,
+                    3930,
+                    2307,
+                    3545,
+                    1501,
+                    3612,
+                    3553,
+                    3797,
+                    2601,
+                    2833,
+                    1954,
+                    2411,
+                    null,
+                    2140,
+                    2597,
+                    2325,
+                    1846,
+                    2359,
+                    3295,
+                    3840,
+                    null,
+                    2744,
+                    1571,
+                    2703,
+                    1967,
+                    2185,
+                    3188];
 
 const input_Form = document.getElementById('stForm');
 const step_dsp = document.getElementById('step_dsp');
 
-if (input_Form && step_dsp) {
-    login_Form.addEventListener('submit', async function (e) {
+step_dsp.style.display = "none";
+
+/* if (input_Form && step_dsp) {
+    input_Form.addEventListener('submit', async function (e) {
         e.preventDefault();
         const formData = new FormData(login_Form).entries()
         const response = await fetch('http://127.0.0.1:5000/api/login', {
@@ -23,21 +84,21 @@ if (input_Form && step_dsp) {
 
         } else {
             console.log(code)
-            if (errM.style.display === "none") {
+            if (errM.style.display == "none") {
                 errM.style.display = "block";
             }
             errM.innerHTML = "Error: " + result["message"];
             
         }
     });
-}
+} */
 
-new Chart("stepsChart", {
+new Chart("weekChart", {
     type: "line",
     data: {
-      labels: Steps,
+      labels: date,
       datasets: [{ 
-        data: date,
+        data: steps,
         borderColor: "red",
         fill: false
       }]
@@ -45,4 +106,19 @@ new Chart("stepsChart", {
     options: {
       legend: {display: false}
     }
-  });
+});
+
+new Chart("monthChart", {
+    type: "line",
+    data: {
+      labels: month,
+      datasets: [{ 
+        data: month_steps,
+        borderColor: "red",
+        fill: false
+      }]
+    },
+    options: {
+      legend: {display: false}
+    }
+});
