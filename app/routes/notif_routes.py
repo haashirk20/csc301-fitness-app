66690ecd458e6cdd0826f8b4a2f311bd3209d1f6
Mohak Varma <116565656+Melon-Man1@users.noreply.github.com>
2024-03-26 03:53:10 -0400

    Ticket 84 Added Notification API

---
+++ app/routes/notif_routes.py
@@ -0,0 +1,16 @@
+from flask import request, session
+from app import app
+from app.models import User
+
+@app.route('/api/notifs', methods=['POST'])
+def set_profile():
+    if "user" not in session:
+        return {'message': 'user not signed in'}, 401
+
+    data = request.get_json()
+    new_notif = data.get('notiftime', '')
+
+    user = User.User(id=session['user']['id'], name=session['user']['name'])
+    user.set_profile(new_notif)
+
+    return {'message': 'success'}, 200
